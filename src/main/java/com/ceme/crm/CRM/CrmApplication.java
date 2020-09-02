2f2aae099fd0fe9b3ee9f56ca80fdc0a0fe7a92c
heebeers <hieu.b.trinh@gmail.com>
2020-09-02 16:36:24 -0500

    added cross origin to main method

--- src/main/java/com/ceme/crm/CRM/CrmApplication.java
+++ src/main/java/com/ceme/crm/CRM/CrmApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@CrossOrigin
 @Import(SpringDataRestConfiguration.class)
 public class CrmApplication {
 
